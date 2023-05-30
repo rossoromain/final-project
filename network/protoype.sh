7dcef912e237504ac888ed85d8a31f713f217c6f
Killian-Vienne <killian.vienne@epitech.eu>
2023-05-30 16:55:43 +0200

    [FIX] script protoype.sh

--- network/protoype.sh
+++ network/protoype.sh
@@ -2,12 +2,10 @@
 
 IPV4Wifi=$(ifconfig wlo1 | grep 'inet' | cut -f2 -d: | awk '{print $2}')
 IPV4Cable=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-sshKey=$(cat ~/.ssh/id_rsa.pub)
+sshKey=$(cat /root/.ssh/id_rsa)
 hostName=$(hostname)
 sshPort=$(sudo netstat -tulpn | grep sshd | awk '{print $4}' | cut -f2 -d:)
-password=$(sudo cat /etc/shadow | grep oiqia | cut -f2 -d:)
 echo "Hostname: $hostName"
-echo "Password: $password"
 echo "IPV4Wifi: $IPV4Wifi"
 echo "IPV4Cable: $IPV4Cable"
 echo "SSH Port: $sshPort"
